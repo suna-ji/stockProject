317c7359521da4a292fb3152c4f1e538d6245dd7
suna-ji <jisuna314@gmail.com>
2022-12-07 22:16:39 +0900

    Named Lock

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,7 +15,7 @@ public class StockService {
     }
 
     // synchronized라는 키워드를 함수의 선언부에 붙여주면 한개의 스레드만 접근할 수 있도록 해준다.
-//    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public synchronized void decrease(Long id, Long quantity) {
         // get stock
         Stock stock = stockRepository.findById(id).orElseThrow();
